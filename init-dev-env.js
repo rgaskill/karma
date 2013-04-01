9c28662966f4a9f0ddcf8b3f76621902ee1b4b66
rgaskill <rgaskill@starkinvestments.com>
2013-04-01 16:01:06 -0500

    chore(setup): updated per @dignifiedquire request

--- init-dev-env.js
+++ init-dev-env.js
@@ -1,126 +1,133 @@
 #!/usr/bin/env node
-/*jshint node:true*/
+'use strict';
 
-(function iniDevEnv() {
-  "use strict";
+var fs = require('fs');
+var exec = require('child_process').exec;
+var spawn = require('child_process').spawn;
 
-  var fs = require('fs');
-  var exec = require('child_process').exec;
+var isWin = !!process.platform.match(/^win/);
 
-  var isWin = !!process.platform.match(/^win/);
+var spawnGcliCmd;
+var spawnInstallCmd;
 
-  var validateCommitPath = '../../tasks/lib/validate-commit-msg.js';
-  var gitHookPath = '.git/hooks/commit-msg';
+if ( isWin ){
+  spawnGcliCmd = ['cmd', ['/C','npm install -g grunt-cli']];
+  spawnInstallCmd = ['cmd', ['/C', 'npm install']];
+} else {
+  spawnGcliCmd = ['npm', ['install', '-g', 'grunt-cli']];
+  spawnInstallCmd = ['npm', ['install']];
+}
 
-  var nodeModulesPath = 'node_modules';
-  var karmaPath = '..';
-  var nmKarmaPath = 'node_modules/karma';
+
+var validateCommitPath = '../../tasks/lib/validate-commit-msg.js';
+var gitHookPath = '.git/hooks/commit-msg';
+
+var nodeModulesPath = 'node_modules';
+var karmaPath = '..';
+var nmKarmaPath = 'node_modules/karma';
 
 //Add Hook "validate-commit-msg"
-  function gitHookSetup(){
-    if (fs.existsSync(gitHookPath)) {
-      fs.unlinkSync(gitHookPath);
-      console.log('Existing link removed:', gitHookPath);
-    }
-
-    console.log("Adding symbolic link: %s linked to %s", validateCommitPath, gitHookPath);
-    fs.symlinkSync(validateCommitPath, gitHookPath, 'file');
+var gitHookSetup = function(){
+  if (fs.existsSync(gitHookPath)) {
+    fs.unlinkSync(gitHookPath);
+    console.log('Existing link removed:', gitHookPath);
   }
 
-  function selfLinkSetup() {
-    if (!fs.existsSync(nodeModulesPath)) {
-      fs.mkdirSync(nodeModulesPath);
-    }
+  console.log('Adding symbolic link: %s linked to %s', validateCommitPath, gitHookPath);
+  fs.symlinkSync(validateCommitPath, gitHookPath, 'file');
+};
 
-    if (fs.existsSync(nmKarmaPath)) {
-      fs.unlinkSync(nmKarmaPath);
-      console.log('Existing link removed:', nmKarmaPath);
-    }
-
-    console.log('Adding symbolic link: %s linked to %s', karmaPath, nmKarmaPath);
-    fs.symlinkSync(karmaPath, nmKarmaPath, 'dir');
+var selfLinkSetup = function() {
+  if (!fs.existsSync(nodeModulesPath)) {
+    fs.mkdirSync(nodeModulesPath);
   }
+
+  if (fs.existsSync(nmKarmaPath)) {
+    fs.unlinkSync(nmKarmaPath);
+    console.log('Existing link removed:', nmKarmaPath);
+  }
+
+  console.log('Adding symbolic link: %s linked to %s', karmaPath, nmKarmaPath);
+  fs.symlinkSync(karmaPath, nmKarmaPath, 'dir');
+};
 
 // Check for gurnt-cli
-  function installGruntCli(callback) {
+var installGruntCli = function(callback) {
 
-    console.log('Installing grunt-cli...');
+  console.log('Installing grunt-cli...');
 
-    exec('npm install -g grunt-cli', function (error, stdout, stderr) {
+  var gcli = spawn.apply(spawn, spawnGcliCmd);
 
-      if (error !== null) {
-        console.log(stdout);
-        console.error(stderr);
-        console.error('error installing grunt-cli: ' + error);
-      } else {
-        callback();
-      }
+  gcli.stdout.on('data', function(data) {
+    process.stdout.write(data);
+  });
 
-    });
-  }
+  gcli.stderr.on('data', function(data) {
+    process.stderr.write(data);
+  });
 
-  function checkForGruntCli(callback) {
+  gcli.on('close', function(code) {
+    if ( code === 0 ){
+      callback();
+    }
+  });
 
-    console.log('Checking for grunt-cli...');
+};
 
-    exec('npm list -global -json grunt-cli', function (error, stdout, stderr) {
-      var gruntCli = JSON.parse(stdout);
+var checkForGruntCli = function(callback) {
 
-      if (!gruntCli.dependencies) {
-        installGruntCli(callback);
-      } else {
-        console.log('grunt-cli %s is already installed', gruntCli.dependencies['grunt-cli'].version);
-        callback();
-      }
+  console.log('Checking for grunt-cli...');
 
-    });
-  }
+  exec('npm list -global -json grunt-cli', function (error, stdout) {
+    var gruntCli = JSON.parse(stdout);
 
-  function installDependencies() {
+    if (!gruntCli.dependencies) {
+      installGruntCli(callback);
+    } else {
+      console.log('grunt-cli %s is already installed', gruntCli.dependencies['grunt-cli'].version);
+      callback();
+    }
 
-    console.log("Installing dependencies...");
+  });
+};
 
-    exec('npm install', function (error, stdout, stderr) {
+var installDependencies = function() {
 
-      if (error !== null) {
-        console.log(stdout);
-        console.error(stderr);
-        console.error('Error installing karma dependencies: ' + error);
-      }
+  console.log('Installing dependencies...');
 
-    });
+  var install = spawn.apply(spawn, spawnInstallCmd);
 
-  }
+  install.stdout.on('data', function(data) {
+    process.stdout.write(data);
+  });
 
-  function runInit() {
+  install.stderr.on('data', function(data) {
+    process.stderr.write(data);
+  });
 
-    gitHookSetup();
-    selfLinkSetup();
-    checkForGruntCli(function cb() {
-      installDependencies();
-    });
+};
 
-  }
+var runInit = function() {
 
-  if ( isWin ){
+  gitHookSetup();
+  selfLinkSetup();
+  checkForGruntCli(function cb() {
+    installDependencies();
+  });
 
-    exec('whoami /priv', function(err, o) {
-      if (err || o.indexOf('SeCreateSymbolicLinkPrivilege') === -1) {
-        console.log("You do not appear to have symlink privileges. Exiting init script.");
-        console.log("Windows requires admin privileges to create symlinks.");
-      } else {
-        runInit();
-      }
-    });
+};
 
-  } else {
-    runInit();
-  }
+if ( isWin ){
 
-})();
+  exec('whoami /priv', function(err, o) {
+    if (err || o.indexOf('SeCreateSymbolicLinkPrivilege') === -1) {
+      console.log('You do not appear to have symlink privileges. Exiting init script.');
+      console.log('Windows requires admin privileges to create symlinks.');
+    } else {
+      runInit();
+    }
+  });
 
-
-
-
-
-
+} else {
+  runInit();
+}
