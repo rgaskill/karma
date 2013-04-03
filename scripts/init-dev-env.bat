4bba2086890c9d41408e3d7d1b5cd68c5a015a95
Roarke Gaskill <roarke.gaskill@gmail.com>
2013-04-02 20:50:22 -0500

    chore(setup): move commands to scripts directory.

--- scripts/init-dev-env.bat
+++ scripts/init-dev-env.bat
@@ -1,1 +1,8 @@
-node scripts/init-dev-env.js
+@echo off
+
+if not exist ".git" (
+  echo Run the init script from the root of the project. e.g.
+  echo   $ scripts/init-dev-env.bat
+) else (
+  node scripts/init-dev-env.js
+)
